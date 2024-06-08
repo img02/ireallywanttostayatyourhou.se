46a019768b2de2a0a6ee0f15d8ac7ad2e54c3c6c
Will <70348218+img02@users.noreply.github.com>
2024-06-08 15:01:03 +1000

    add image

--- src/MapData.jsx
+++ src/MapData.jsx
@@ -3,12 +3,14 @@ import Azys_Lla from "./assets/images/Azys_Lla.jpg";
 import Central_Shroud from "./assets/images/Central_Shroud.jpg";
 import Central_Shroud_orig from "./assets/images/Central_Shroud-Orig.jpg";
 import Central_Thanalan from "./assets/images/Central_Thanalan.jpg";
+import Upper_La_Noscea from "./assets/images/Upper_La_Noscea.jpg";
 const MapData = [
   { name: "Amh_Araeng", src: Amh_Araeng, id: 815 },
   { name: "Azys_Lla", src: Azys_Lla, id: 402 },
   { name: "Central_Shroud", src: Central_Shroud, id: 148 },
   { name: "Central_Shroud_orig", src: Central_Shroud_orig, id: 1480 },
   { name: "Central_Thanalan", src: Central_Thanalan, id: 141 },
+  { name: "Upper_La_Noscea", src: Upper_La_Noscea, id: 139 },
 ];
 
 export default MapData;
