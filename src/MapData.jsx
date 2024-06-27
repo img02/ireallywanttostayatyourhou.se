1043434af486efe11f8153d5330389bcd9679051
Will <70348218+imaginary-png@users.noreply.github.com>
2024-06-27 14:04:03 +1000

    add dawntrail maps

--- src/MapData.jsx
+++ src/MapData.jsx
@@ -7,8 +7,14 @@ import Upper_La_Noscea from "./assets/images/Upper_La_Noscea.jpg";
 import Thavnair from "./assets/images/Thavnair.jpg";
 import The_Raktika_Greatwood from "./assets/images/The_Rak'tika_Greatwood.jpg";
 import Garlemald from "./assets/images/Garlemald.jpg";
+import Heritage_Found from "./assets/images/Heritage_Found-data.jpg";
+import Kozamauka from "./assets/images/Kozama'uka-data.jpg";
+import Living_Memory from "./assets/images/Living_Memory-data.jpg";
+import Shaaloani from "./assets/images/Shaaloani-data.jpg";
+import Urqopacha from "./assets/images/Urqopacha-data.jpg";
+import Yak_Tel from "./assets/images/Yak_T'el-data.jpg";
 
-const MapData = [
+const TestData = [
   { name: "Amh_Araeng", src: Amh_Araeng, id: 815 },
   { name: "Azys_Lla", src: Azys_Lla, id: 402 },
   { name: "Central_Shroud", src: Central_Shroud, id: 148 },
@@ -18,6 +24,15 @@ const MapData = [
   { name: "Thavnair", src: Thavnair, id: 957 },
   { name: "The_Raktika_Greatwood", src: The_Raktika_Greatwood, id: 817 },
   { name: "Garlemald", src: Garlemald, id: 817 },
+];
+
+const MapData = [
+  { name: "Urqopacha", src: Urqopacha, id: 1187 },
+  { name: "Kozamauka", src: Kozamauka, id: 1188 },
+  { name: "Yak_Tel", src: Yak_Tel, id: 1189 },
+  { name: "Shaaloani", src: Shaaloani, id: 1190 },
+  { name: "Heritage_Found", src: Heritage_Found, id: 1191 },
+  { name: "Living_Memory", src: Living_Memory, id: 1192 },
 ];
 
 export default MapData;
